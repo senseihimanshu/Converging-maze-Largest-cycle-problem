f442ba6d3c654a0c944fef5bada83f1c9260846b
Himanshu Sharma <hsharma151197@gmail.com>
2019-12-03 23:57:12 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int findingLargestCycle(int noOfInputs, int *edges){
+    int largestCycle = 0;
+    for(int i=0; i<noOfInputs; i++){
+        int currentEdge = edges[i];
+        int count = currentEdge;
+        int nextEdge = edges[currentEdge];
+
+        int n = 0;
+        while(currentEdge != nextEdge && n<noOfInputs+1){
+            if(nextEdge == -1 || currentEdge == -1){
+                count = 0;
+                break;
+            }
+
+            if(nextEdge != currentEdge){
+                count += nextEdge;
+            }
+
+            nextEdge = edges[nextEdge];
+            n++;
+        }
+
+        if(count > largestCycle && n != noOfInputs+1){
+            largestCycle = count;
+        }
+    }
+    return largestCycle;
+}
+
+int main(){
+    int testCases;
+    scanf("%d", &testCases);
+    int numberOfInputs;
+    scanf("%d", &numberOfInputs);
+    int edges[numberOfInputs];
+    for(int i=0; i<numberOfInputs; i++){
+        scanf("%d", &edges[i]);
+    }
+
+    printf("%d", findingLargestCycle(numberOfInputs, &edges[0]));
+}
+
